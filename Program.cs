b4069923bc5c2eed841bac4639a596858fe9d3e1
Anton Krylatov <serverbsvvv@gmail.com>
2026-09-15 09:56:35 +0300

    Lab01: System monitor

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine($"Komp: {Environment.MachineName}");
+﻿/*Console.WriteLine($"Komp: {Environment.MachineName}");
 Console.WriteLine($"user: {Environment.UserName}");
 Console.WriteLine($"data i vrema: {DateTime.Now:dd.MM.yyyy HH:mm}");
 
@@ -10,4 +10,31 @@ Console.WriteLine($"logic processors: {Environment.ProcessorCount}");
 Console.WriteLine($"PID: {Environment.ProcessId}");
 long mem = Environment.WorkingSet / 1024 / 1024;
 Console.WriteLine($"memory: {mem}");
-Console.ReadLine();
+Console.ReadLine();*/
+
+using System.Diagnostics;
+
+Console.WriteLine("mini monitor systema\n");
+Console.WriteLine($"komp: {Environment.MachineName}");
+Console.WriteLine($"user: {Environment.UserName}");
+Console.WriteLine($"OC: {Environment.OSVersion}");
+Console.WriteLine($"64 bit? {Environment.Is64BitOperatingSystem}");
+Console.WriteLine($"logic processors: {Environment.ProcessorCount}");
+
+Process currentProcess = Process.GetCurrentProcess();
+
+Console.WriteLine($"pid: {currentProcess.Id}");
+Console.WriteLine($"memory:");
+for (int i = 1; i <= 3; i++)
+{
+    currentProcess.Refresh();
+    long mem = currentProcess.WorkingSet64 / 1024 / 1024;
+    Console.WriteLine($"{i}: {mem} MB");
+    if (i < 3)
+    {
+        Console.WriteLine("update....");
+        Console.ReadLine();
+    }
+}
+
+Console.WriteLine("done");
