f0775ca27d37a5bb5f942c3f8fe22f31888b89e6
Anton Krylatov <serverbsvvv@gmail.com>
2026-09-15 09:39:44 +0300

    Lab01:systeminfo investigations

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿Console.WriteLine($"Komp: {Environment.MachineName}");
 Console.WriteLine($"user: {Environment.UserName}");
 Console.WriteLine($"data i vrema: {DateTime.Now:dd.MM.yyyy HH:mm}");
+
+Console.WriteLine($"OC: {Environment.OSVersion}");
+Console.WriteLine($"64? {Environment.Is64BitOperatingSystem}");
+
+Console.WriteLine($"logic processors: {Environment.ProcessorCount}");
+
+Console.WriteLine($"PID: {Environment.ProcessId}");
+long mem = Environment.WorkingSet / 1024 / 1024;
+Console.WriteLine($"memory: {mem}");
+Console.ReadLine();
